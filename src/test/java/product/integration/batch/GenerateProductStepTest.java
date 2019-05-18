8b0e258be1f8dc234c9787cf29eb3eec649f49de
ddingcham <qo0op308@gmail.com>
2019-05-18 12:01:38 +0900

    Validate Test Bean Configuration : for Fixture

--- src/test/java/product/integration/batch/GenerateProductStepTest.java
+++ src/test/java/product/integration/batch/GenerateProductStepTest.java
@@ -1,19 +1,31 @@
 package product.integration.batch;
 
 import lombok.extern.slf4j.Slf4j;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.batch.core.BatchStatus;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.test.StepRunner;
 import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import product.domain.Product;
+import product.integration.batch.support.IdGenerator;
+
+import java.math.BigInteger;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -28,6 +40,30 @@ public class GenerateProductStepTest implements ApplicationContextAware {
     private ApplicationContext context;
 
     private StepRunner stepRunner;
+
+    @Autowired
+    private JobLauncher jobLauncher;
+
+    @Autowired
+    private JobRepository jobRepository;
+
+    @Autowired
+    private IdGenerator idGenerator;
+
+    @Autowired
+    private Map<BigInteger, Product> mockStore;
+
+    @Before
+    public void setUp() {
+        stepRunner = new StepRunner(jobLauncher, jobRepository);
+    }
+
+    @Test
+    public void temp_validate_fixture() {
+        assertThat(stepRunner).isNotNull();
+        assertThat(idGenerator).isNotNull();
+        assertThat(mockStore).isNotNull();
+    }
 
     @Test
     public void validate_num_of_products() {
@@ -49,5 +85,36 @@ public class GenerateProductStepTest implements ApplicationContextAware {
     @EnableBatchProcessing
     static class TempStepDefinition {
 
+        // Mock Mongo(Key-Value Document Store)
+        @Bean
+        public Map<BigInteger, Product> mockStore() {
+            return new ConcurrentHashMap<>();
+        }
+
+        @Bean
+        public IdGenerator idGenerator() {
+            return new MockIdGenerator();
+        }
+
+    }
+
+    static class MockIdGenerator implements IdGenerator {
+
+        private long INITIAL_VALUE = 1L;
+        private AtomicLong current;
+
+        public MockIdGenerator() {
+            current = new AtomicLong(INITIAL_VALUE);
+        }
+
+        @Override
+        public long next() {
+            return current.getAndIncrement();
+        }
+
+        @Override
+        public void reset() {
+            current.set(INITIAL_VALUE);
+        }
     }
 }
