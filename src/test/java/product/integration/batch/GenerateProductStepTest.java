bf08cc83f9322033d6e1c3e7d672b8e1c1c6f8bd
ddingcham <qo0op308@gmail.com>
2019-05-18 12:08:59 +0900

    Define Temp GenerateProductStep : Test Pass

--- src/test/java/product/integration/batch/GenerateProductStepTest.java
+++ src/test/java/product/integration/batch/GenerateProductStepTest.java
@@ -7,31 +7,47 @@ import org.junit.runner.RunWith;
 import org.springframework.batch.core.BatchStatus;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.LineMapper;
+import org.springframework.batch.item.file.mapping.DefaultLineMapper;
+import org.springframework.batch.item.file.mapping.FieldSetMapper;
+import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
+import org.springframework.batch.item.file.transform.LineTokenizer;
 import org.springframework.batch.test.StepRunner;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import product.domain.Product;
+import product.domain.support.ProductNo;
+import product.domain.support.ProductOrigin;
+import product.domain.support.ProductProperties;
 import product.integration.batch.support.IdGenerator;
 
 import java.math.BigInteger;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicLong;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @Slf4j
 @ContextConfiguration(classes = {GenerateProductStepTest.TempStepDefinition.class})
-@TestPropertySource(properties = "rawProductPath=/test-dataset/test-product/set")
+@TestPropertySource(properties = "rawProductPath=/test-dataset/test-product-set")
 @RunWith(SpringJUnit4ClassRunner.class)
 public class GenerateProductStepTest implements ApplicationContextAware {
 
@@ -55,14 +71,9 @@ public class GenerateProductStepTest implements ApplicationContextAware {
 
     @Before
     public void setUp() {
+        mockStore.clear();
+        idGenerator.reset();
         stepRunner = new StepRunner(jobLauncher, jobRepository);
-    }
-
-    @Test
-    public void temp_validate_fixture() {
-        assertThat(stepRunner).isNotNull();
-        assertThat(idGenerator).isNotNull();
-        assertThat(mockStore).isNotNull();
     }
 
     @Test
@@ -85,6 +96,8 @@ public class GenerateProductStepTest implements ApplicationContextAware {
     @EnableBatchProcessing
     static class TempStepDefinition {
 
+        private static final int DEFAULT_CHUNK_SIZE = 10;
+
         // Mock Mongo(Key-Value Document Store)
         @Bean
         public Map<BigInteger, Product> mockStore() {
@@ -96,11 +109,72 @@ public class GenerateProductStepTest implements ApplicationContextAware {
             return new MockIdGenerator();
         }
 
+        @Bean
+        public Step generateProducts(StepBuilderFactory stepBuilderFactory, ItemReader<Product> rawProductReader, ItemWriter<Product> rawProductWriter) {
+            return stepBuilderFactory
+                    .get("generateProducts")
+                    .<Product, Product>chunk(DEFAULT_CHUNK_SIZE)
+                    .reader(rawProductReader)
+                    .writer(rawProductWriter)
+                    .build();
+        }
+
+        @Bean
+        public ItemReader<Product> rawProductReader(@Value("${rawProductPath}") Resource rawProducts, LineMapper<Product> rawProductLineMapper) {
+            FlatFileItemReader<Product> rawProductReader = new FlatFileItemReader<>();
+
+            rawProductReader.setResource(rawProducts);
+            rawProductReader.setLineMapper(rawProductLineMapper);
+
+            return rawProductReader;
+        }
+
+        @Bean
+        public LineMapper<Product> rawProductLineMapper(LineTokenizer rawProductTokenizer, FieldSetMapper<Product> rawProductEntityMapper) {
+            DefaultLineMapper<Product> productLineMapper = new DefaultLineMapper<>();
+
+            productLineMapper.setLineTokenizer(rawProductTokenizer);
+            productLineMapper.setFieldSetMapper(rawProductEntityMapper);
+
+            return productLineMapper;
+        }
+
+        @Bean
+        public LineTokenizer rawProductTokenizer() {
+            DelimitedLineTokenizer tokenizer = new DelimitedLineTokenizer(DelimitedLineTokenizer.DELIMITER_TAB);
+            tokenizer.setNames("productNo", "origin", "recommendScore");
+            tokenizer.setIncludedFields(0, 1, 2);
+            return tokenizer;
+        }
+
+        @Bean
+        public FieldSetMapper<Product> rawProductEntityMapper() {
+            return fieldSet -> {
+                ProductOrigin origin = ProductOrigin.of(fieldSet.readString("origin"));
+                return Product.builder()
+                        .productNo(new ProductNo(fieldSet.readLong("productNo")))
+                        .origin(origin)
+                        .productProperties(ProductProperties
+                                .builder(origin)
+                                .build())
+                        .recommendScore(fieldSet.readInt("recommendScore"))
+                        .build();
+            };
+        }
+
+        @Bean
+        public ItemWriter<Product> mockProductWriter(Map<BigInteger, Product> mockStore, IdGenerator idGenerator) {
+            return items -> mockStore.putAll(items
+                    .stream()
+                    .peek((product) -> product.setId(BigInteger.valueOf(idGenerator.next())))
+                    .collect(Collectors.toMap(Product::getId, Function.identity())));
+        }
+
     }
 
     static class MockIdGenerator implements IdGenerator {
 
-        private long INITIAL_VALUE = 1L;
+        private long INITIAL_VALUE = 0L;
         private AtomicLong current;
 
         public MockIdGenerator() {
