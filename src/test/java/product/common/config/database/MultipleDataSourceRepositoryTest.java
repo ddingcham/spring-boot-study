2e1cb8b545390c184b9d35b81cab70b68d797e91
ddingcham <qo0op308@gmail.com>
2019-06-08 13:28:20 +0900

    Define TODO

--- src/test/java/product/common/config/database/MultipleDataSourceRepositoryTest.java
+++ src/test/java/product/common/config/database/MultipleDataSourceRepositoryTest.java
@@ -8,6 +8,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
+import product.domain.book.Book;
 import product.domain.book.BookRepository;
 import product.domain.store.StoreRepository;
 
@@ -32,10 +33,14 @@ public class MultipleDataSourceRepositoryTest {
 }
 
 
-/*
-    TODO
-    When @DataJpaTest
-    Parameter 0 of method testEntityManager in org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManagerAutoConfiguration required a single bean, but 2 were found:
-    For Test Configuration - singular
+//    TODO - Study - Test 환경에서 실행
+//    1. Configuration 패키지/네이밍 정리
+//    2. readable한 store를 가저와서 에약(book)을 만드(editable)는 케이스
+//    3. editable한 book 을 가지고 트랜잭션 나오는 케이스
 
- */
+//  TODO - next Week
+//  1. db 환경 설정하기 - docker
+//  2. 일단은 간단하게 계정 베이스로 readonly / editable 나눠서 테스트
+//  3. 간단한 api 만들기
+//  4. unit test
+//  5. end-to-end test
